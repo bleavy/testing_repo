ff0105df445f09f3242838e961ccd061dc8e590f
Ben <bleavy@Sev1-bEzxUnPVdz.corp.sev1tech.us>
2023-05-21 17:57:54 -0600

    m

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh jenkins@leavytestvm.westus2.cloudapp.azure.com "rpm -qa | grep -i nginx > strace.txt;echo '$(cat strace.txt) is installed'"
+ssh jenkins@leavytestvm.westus2.cloudapp.azure.com "rpm -qa | grep -i nginx > strace.txt;cat strace.txt"
