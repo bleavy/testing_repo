27a30a359948033d6292ebf1192723fc25f622d0
Ben <bleavy@Sev1-bEzxUnPVdz.corp.sev1tech.us>
2023-05-21 17:52:14 -0600

    testagain2

--- test.sh
+++ test.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-ssh jenkins@leavytestvm.westus2.cloudapp.azure.com "
-
-sudo yum install -y nginx;
-
-rpm -qa | grep -i nxginx >> strace.txt;
-
-echo "$(cat strace.txt) is installed""
+ssh jenkins@leavytestvm.westus2.cloudapp.azure.com "rpm -qa | grep -i nxginx > strace.txt;echo '$(cat strace.txt) is installed'"
