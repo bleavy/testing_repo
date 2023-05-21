4c6cc7c7f080f26a42195bd52785bad122cb4f5d
Ben <bleavy@Sev1-bEzxUnPVdz.corp.sev1tech.us>
2023-05-21 17:33:42 -0600

    testagain

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ssh jenkins@leavytestvm.westus.cloudapp.azure.com "
 
-sudo yum install -y strace
+sudo yum install -y strace;
 
-rpm -qa | grep -i strace >> strace.txt
+rpm -qa | grep -i strace >> strace.txt;
 
-echo "$(cat strace) is installed)"; done"
+echo "$(cat strace.txt) is installed""
